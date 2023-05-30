a4fd6587e4132489b404c429f31e8f1ce5696841
noerbarry <noerbarry@gmail.com>
2023-05-30 08:56:54 +0700

    Update pabaraianalytics.py

--- pabaraianalytics.py
+++ pabaraianalytics.py
@@ -175,7 +175,7 @@ def show_main_menu(user):
                     plt.savefig(img_buffer, format='png')
                     img_buffer.seek(0)
         
-       elif chart_type == 'Heatmap':
+    elif chart_type == 'Heatmap':
         st.subheader('Heatmap')
         uploaded_file = st.file_uploader('Unggah file CSV', type=['csv'])
         if uploaded_file is not None:
@@ -199,7 +199,7 @@ def show_main_menu(user):
 
                 # Mengunduh grafik
                 st.markdown("### Download Grafik")
-                download_chart(fig, 'heatmap.png')            
+                download_chart(fig, 'heatmap.png')    
 
 
         elif chart_type == 'Plotly Chart':
