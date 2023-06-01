1f2eb86d97f139392e690084578b4066496404d3
noerbarry <noerbarry@gmail.com>
2023-06-01 12:42:23 +0700

    Update pabaraianalytics.py

--- pabaraianalytics.py
+++ pabaraianalytics.py
@@ -94,6 +94,24 @@ def get_download_link(file_path):
     base64_encoded = base64.b64encode(contents).decode('utf-8')
     href = f'<a href="data:file/png;base64,{base64_encoded}" download="wordcloud.png">Klik di sini untuk mengunduh gambar word cloud</a>'
     return href
+
+# Fungsi untuk membuat grafik Prophet
+def create_prophet_chart(data):
+    # Membuat objek Prophet
+    model = Prophet()
+
+    # Melatih model Prophet dengan data
+    model.fit(data)
+
+    # Membuat dataframe untuk prediksi masa depan
+    future = model.make_future_dataframe(periods=365)
+
+    # Melakukan prediksi dengan model Prophet
+    forecast = model.predict(future)
+
+    # Menampilkan grafik prediksi
+    fig = model.plot(forecast)
+    return fig
       
 # Fungsi untuk mengunduh grafik
 def download_chart(chart, filename):
@@ -117,7 +135,7 @@ def show_main_menu(user):
             logout()
     elif menu == 'Grafik':
         st.subheader('Pilih Jenis Grafik')
-        chart_type = st.selectbox('Jenis Grafik', ['Line Chart', 'Bar Chart', 'Histogram', 'Word Cloud', 'Scatter Plot', 'Barfi Chart', 'Plotly Chart'])
+        chart_type = st.selectbox('Jenis Grafik', ['Line Chart', 'Bar Chart', 'Histogram', 'Word Cloud', 'Scatter Plot', 'Barfi Chart','Prophet Chart', 'Plotly Chart'])
         if chart_type == 'Line Chart':
             st.subheader('Grafik Line')
             uploaded_file = st.file_uploader('Unggah file CSV', type=['csv'])
@@ -304,6 +322,25 @@ def show_main_menu(user):
                      b64_chart = base64.b64encode(img_buffer.read()).decode()
                      href = f'<a href="data:image/png;base64,{b64_chart}" download="barfi_chart.png">Unduh Grafik</a>'
                      st.write(href, unsafe_allow_html=True)
+
+        elif chart_type == 'Prophet Chart':
+            st.subheader('Grafik Prophet')
+            uploaded_file = st.file_uploader('Unggah file CSV', type=['csv'])
+            if uploaded_file is not None:
+                data = pd.read_csv(uploaded_file, delimiter=';')
+                st.dataframe(data)
+
+                # Jika tombol "Tampilkan Grafik" ditekan
+                if st.button('Tampilkan Grafik'):
+                    # Membuat grafik Prophet
+                    fig = create_prophet_chart(data)
+
+                    # Menampilkan grafik di layar menggunakan st.plotly_chart()
+                    st.plotly_chart(fig)
+
+                    # Mengunduh grafik
+                    st.markdown("### Download Grafik")
+                    download_chart(fig, 'prophet_chart.png') 
 
         elif chart_type == 'Plotly Chart':
             st.subheader('Plotly Chart')
